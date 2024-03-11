34441a30dc5d6ec773186184113767ba45657cce
Mădălin-Florin GOIAN <79133273+MadalinGOIAN@users.noreply.github.com>
2024-03-11 20:48:57 +0200

    Creaza baza de date in Azure Mysql

--- BazaDeDate/creare_tabele.sql
+++ BazaDeDate/creare_tabele.sql
@@ -1,57 +1,49 @@
-CREATE SCHEMA `db_licenta`;
+CREATE SCHEMA `bd_licenta` ;
 
-CREATE TABLE `db_licenta`.`utilizatori` (
-  `nume_utilizator` VARCHAR(30) NOT NULL,
-  `hash_parola` VARCHAR(45) NOT NULL,
-  `prenume` VARCHAR(45) NOT NULL,
-  `nume_familie` VARCHAR(45) NOT NULL,
-  `sex` CHAR(1) NOT NULL,
-  `varsta` INT UNSIGNED NOT NULL,
-  `inaltime` INT UNSIGNED NOT NULL,
-  `greutate` INT UNSIGNED NOT NULL,
-  `nivel_activitate_fizica` INT UNSIGNED NOT NULL,
-  `necesar_caloric` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`nume_utilizator`));
+CREATE TABLE `bd_licenta`.`utilizatori` (
+  `nume_utilizator` varchar(30) NOT NULL,
+  `hash_parola` varchar(45) NOT NULL,
+  `prenume` varchar(45) NOT NULL,
+  `nume_familie` varchar(45) NOT NULL,
+  `sex` char(1) NOT NULL,
+  `varsta` int unsigned NOT NULL,
+  `inaltime` int unsigned NOT NULL,
+  `greutate` int unsigned NOT NULL,
+  `nivel_activitate_fizica` int unsigned NOT NULL,
+  `necesar_caloric` int unsigned NOT NULL,
+  PRIMARY KEY (`nume_utilizator`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `db_licenta`.`utilizatori_conectati` (
-  `nume_utilizator_conectat` VARCHAR(30) NOT NULL,
-  `hash_parola` VARCHAR(45) NOT NULL,
+CREATE TABLE `bd_licenta`.`utilizatori_conectati` (
+  `nume_utilizator_conectat` varchar(30) NOT NULL,
+  `hash_parola` varchar(45) NOT NULL,
   PRIMARY KEY (`nume_utilizator_conectat`),
-  CONSTRAINT `fk_nume_utilizator`
-    FOREIGN KEY (`nume_utilizator_conectat`)
-    REFERENCES `db_licenta`.`utilizatori` (`nume_utilizator`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  CONSTRAINT `fk_nume_utilizator` FOREIGN KEY (`nume_utilizator_conectat`) REFERENCES `bd_licenta`.`utilizatori` (`nume_utilizator`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `db_licenta`.`alimente` (
-  `denumire` VARCHAR(30) NOT NULL,
-  `cod_bare` VARCHAR(13) NULL,
-  `calorii` INT NOT NULL,
-  `grasimi` FLOAT NOT NULL,
-  `glucide` FLOAT NOT NULL,
-  `proteine` FLOAT NOT NULL,
-  PRIMARY KEY (`denumire`));
+CREATE TABLE `bd_licenta`.`alimente` (
+  `denumire` varchar(30) NOT NULL,
+  `cod_bare` varchar(13) DEFAULT NULL,
+  `calorii` int NOT NULL,
+  `grasimi` float NOT NULL,
+  `glucide` float NOT NULL,
+  `proteine` float NOT NULL,
+  PRIMARY KEY (`denumire`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `db_licenta`.`istoric` (
-  `istoric_id` INT NOT NULL AUTO_INCREMENT,
-  `nume_utilizator` VARCHAR(30) NOT NULL,
-  `denumire_aliment` VARCHAR(30) NOT NULL,
-  `data` DATE NOT NULL,
-  `cantitate_consumata` FLOAT NOT NULL,
-  `calorii_consumate` INT NOT NULL,
-  `grasimi_consumate` FLOAT NOT NULL,
-  `glucide_consumate` FLOAT NOT NULL,
-  `proteine_consumate` FLOAT NOT NULL,
+CREATE TABLE `bd_licenta`.`istoric` (
+  `istoric_id` int NOT NULL AUTO_INCREMENT,
+  `nume_utilizator` varchar(30) NOT NULL,
+  `denumire_aliment` varchar(30) NOT NULL,
+  `data` date NOT NULL,
+  `cantitate_consumata` float NOT NULL,
+  `calorii_consumate` int NOT NULL,
+  `grasimi_consumate` float NOT NULL,
+  `glucide_consumate` float NOT NULL,
+  `proteine_consumate` float NOT NULL,
   PRIMARY KEY (`istoric_id`),
-  INDEX `fk_nume_utilizator_idx` (`nume_utilizator` ASC) VISIBLE,
-  INDEX `fk_denumire_aliment_idx` (`denumire_aliment` ASC) VISIBLE,
-  CONSTRAINT `fk_istoric_nume_utilizator`
-    FOREIGN KEY (`nume_utilizator`)
-    REFERENCES `db_licenta`.`utilizatori` (`nume_utilizator`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_denumire_aliment`
-    FOREIGN KEY (`denumire_aliment`)
-    REFERENCES `db_licenta`.`alimente` (`denumire`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  KEY `fk_nume_utilizator_idx` (`nume_utilizator`),
+  KEY `fk_denumire_aliment_idx` (`denumire_aliment`),
+  CONSTRAINT `fk_denumire_aliment` FOREIGN KEY (`denumire_aliment`) REFERENCES `bd_licenta`.`alimente` (`denumire`),
+  CONSTRAINT `fk_istoric_nume_utilizator` FOREIGN KEY (`nume_utilizator`) REFERENCES `bd_licenta`.`utilizatori` (`nume_utilizator`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
