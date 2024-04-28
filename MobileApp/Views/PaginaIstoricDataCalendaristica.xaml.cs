9329f51fe950b853ddea717a8d1f8b0b94f5f35a
Mădălin-Florin GOIAN <79133273+MadalinGOIAN@users.noreply.github.com>
2024-04-28 20:22:55 +0300

    Creeaza pagina principala

---
+++ MobileApp/Views/PaginaIstoricDataCalendaristica.xaml.cs
@@ -0,0 +1,9 @@
+namespace MobileApp.Views;
+
+public partial class PaginaIstoricDataCalendaristica : ContentPage
+{
+	public PaginaIstoricDataCalendaristica()
+	{
+		InitializeComponent();
+	}
+}
--- MobileApp/Views/PaginaPrincipala.xaml.cs
+++ MobileApp/Views/PaginaPrincipala.xaml.cs
@@ -6,4 +6,9 @@ public partial class PaginaPrincipala : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void MaiMulteBtn_Clicked(object sender, EventArgs e)
+    {
+		Application.Current.MainPage = new PaginaIstoricDataCalendaristica();
+    }
 }
